7c81744c2343958c662a6d6c784f5902fdf059e8
s3blag <226173@student.pwr.edu.pl>
2017-11-06 13:12:27 +0100

    Odejmowanie min od kolumn i wierszy

--- Project_1/BranchAndBound.cs
+++ Project_1/BranchAndBound.cs
@@ -8,6 +8,67 @@ namespace Project_1
 {
     abstract class BranchAndBound
     {
+        private const int INF = Int32.MaxValue;
+
+        public static int[,] ReduceMatrix(int[,] matrix)
+        {
+            ReduceRow(matrix);
+            ReduceColumn(matrix);
+
+            return matrix;
+        }
+
+        private static void ReduceRow(int[,] matrix)
+        {
+            int min;
+
+            for (int i = 0; i < 4; i++)
+            {
+                min = INF;
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    if (matrix[i, j] < min)
+                    {
+                        min = matrix[i, j];
+                    }
+                }
+
+                if (min != 0)
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (matrix[i, j] != INF)
+                            matrix[i, j] -= min;
+                    }
+            }
+        }
+
+        private static void ReduceColumn(int[,] matrix)
+        {
+            int min;
+
+            for (int i = 0; i < 4; i++)
+            {
+                min = INF;
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    if (matrix[j, i] < min)
+                    {
+                        min = matrix[j, i];
+                    }
+                }
+
+                if (min != 0)
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (matrix[j, i] != INF)
+                            matrix[j, i] -= min;
+                    }
+            }
+        }
 
     }
 }
