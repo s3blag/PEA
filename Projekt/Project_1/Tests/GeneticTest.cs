798f82afffdf6ff9e64dfd3ec51e83f7bf814e5d
lukasz-zatorski <lukasz91112@gmail.com>
2018-01-18 17:44:26 +0100

    klasa do testow

---
+++ Projekt/Project_1/Tests/GeneticTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1.Tests
+{
+    static internal class GeneticTest
+    {
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Zapis do pliku
+        /// </summary>
+        /// <param name="path"> Ścieżka pliku wyjściowego </param>
+        /// <param name="output"> Dane do zapisania </param>
+        private static void WriteOutputToFile(string path, string output)
+        {
+            FileStream fs = new FileStream(path,
+                FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            try
+            {
+                StreamWriter sw = new StreamWriter(fs);
+
+                sw.WriteLine(output);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        #endregion
+
+    }
+}
